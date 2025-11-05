6113bc5db750e6ed274e0e39cd01ca700998b858
ichin-tan <chintanpanchal396@gmail.com>
2025-11-04 19:34:14 -0500

    Initial Commit

---
+++ Avomeals/AvomealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvomealsApp.swift
+//  Avomeals
+//
+//  Created by CP on 04/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AvomealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
